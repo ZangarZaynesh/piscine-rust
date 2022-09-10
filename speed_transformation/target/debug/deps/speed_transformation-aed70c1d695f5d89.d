30486d7626302b81613161991c061e2dc896dd81
zangarzaynesh <zorgee012094@gmail.com>
2022-09-10 22:08:47 +0600

    speed_transformation/

---
+++ speed_transformation/target/debug/deps/speed_transformation-aed70c1d695f5d89.d
@@ -0,0 +1,5 @@
+/home/zangar/Rust/piscine-rust/speed_transformation/target/debug/deps/speed_transformation-aed70c1d695f5d89: src/main.rs
+
+/home/zangar/Rust/piscine-rust/speed_transformation/target/debug/deps/speed_transformation-aed70c1d695f5d89.d: src/main.rs
+
+src/main.rs:
---
+++ speed_transformation/target/debug/speed_transformation.d
@@ -0,0 +1,1 @@
+/home/zangar/Rust/piscine-rust/speed_transformation/target/debug/speed_transformation: /home/zangar/Rust/piscine-rust/speed_transformation/src/main.rs
