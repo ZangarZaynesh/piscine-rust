b23dcc49edaa0419ca40734486ef86f3582ea9c1
zangarzaynesh <zorgee012094@gmail.com>
2022-09-11 21:13:52 +0600

    groceries/

---
+++ groceries/target/debug/deps/groceries-b6837a008f8ce2a3.d
@@ -0,0 +1,5 @@
+/home/zangar/Rust/piscine-rust/groceries/target/debug/deps/groceries-b6837a008f8ce2a3: src/main.rs
+
+/home/zangar/Rust/piscine-rust/groceries/target/debug/deps/groceries-b6837a008f8ce2a3.d: src/main.rs
+
+src/main.rs:
---
+++ groceries/target/debug/groceries.d
@@ -0,0 +1,1 @@
+/home/zangar/Rust/piscine-rust/groceries/target/debug/groceries: /home/zangar/Rust/piscine-rust/groceries/src/main.rs
